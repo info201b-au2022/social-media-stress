85c55057e5d07ee7ef7da9a21f48288dd2d3cf97
Wesley <serpents123@gmail.com>
2022-11-12 23:37:10 -0800

    reddit dataset: add subreddit and replace 0 and 1

--- data/drop_test_cols.r
+++ data/drop_test_cols.r
@@ -1,17 +1,20 @@
 library("dplyr")
+library("stringr")
 
-dreaddit_test <- read.csv("https://raw.githubusercontent.com/info201b-au2022/Project-11-BC/main/data/dreaddit-test.csv")
+dreaddit_test <- read.csv("https://raw.githubusercontent.com/info201b-au2022/Project-11-BC/main/data/raw/dreaddit-test.csv")
+
+dreaddit_test <- dreaddit_test[, -1]
 
 dreaddit_test_clean <- select(
-  dreaddit_test, text, label,
+  dreaddit_test, subreddit, text, label,
   confidence, social_karma, lex_liwc_Clout, lex_liwc_Authentic,
   lex_liwc_Tone, lex_liwc_posemo, lex_liwc_negemo, lex_liwc_anx,
   lex_liwc_anger, lex_liwc_sad, lex_liwc_power, sentiment
 )
 
 dreaddit_test_clean <- rename(dreaddit_test_clean,
-  "Reddit Text" = "text", "Stress (1) or Not Stress (0)" = "label",
-  "Model Confidence in Accuracy" = "confidence",
+  "Subreddit" = "subreddit", "Reddit Text" = "text", "Stress" = "label",
+  "Model Confidence" = "confidence",
   "Reddit Karma" = "social_karma", "Clout" = "lex_liwc_Clout",
   "Authenticity" = "lex_liwc_Authentic", "Tone" = "lex_liwc_Tone",
   "Positive Emotion" = "lex_liwc_posemo",
@@ -20,5 +23,9 @@ dreaddit_test_clean <- rename(dreaddit_test_clean,
   "Sadness" = "lex_liwc_sad", "Power" = "lex_liwc_power",
   "Sentiment" = "sentiment"
 )
-print(getwd())
-write.csv(dreaddit_test_clean, paste(getwd(), "/data/Dreaddit Dataset.csv", sep =""))
+
+dreaddit_test_clean <- dreaddit_test_clean %>%
+  mutate(Stress = str_replace(Stress, "0", "Not Stressed")) %>%
+  mutate(Stress = str_replace(Stress, "1", "Stressed"))
+
+write.csv(dreaddit_test_clean, paste(getwd(), "/data/Dreaddit Dataset.csv", sep =""), row.names = FALSE)
