43586de1f74e8afbee8ca636a894055aa748ae0d
katiestevens24 <katsierr@uw.edu>
2022-11-13 20:10:00 -0800

    add chart #1

---
+++ source/chart1.R
@@ -0,0 +1,19 @@
+# load packages
+library("dplyr")
+library("tidyr")
+library("ggplot2")
+
+#load csv data
+dreaddit <- read.csv("https://raw.githubusercontent.com/info201b-au2022/Project-11-BC/main/data/Dreaddit%20Dataset.csv", stringsAsFactors = FALSE)
+
+# filter data
+dreaddit_data <-dreaddit %>%
+  group_by(Subreddit) %>%
+  summarize(Tone = mean(Tone), na.rm = TRUE)
+
+#create scatterplot 
+ggplot(data = dreaddit_data) +
+  geom_point(mapping = aes(x = Subreddit, y = Tone)) +
+  coord_flip() +
+  labs(title = "Average Tone by Subreddit") 
+
