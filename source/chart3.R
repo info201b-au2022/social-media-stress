5a51aca769e46d13d410ce2a89aaacfa09e660c7
katiestevens24 <katsierr@uw.edu>
2022-11-14 10:14:58 -0800

    add chart 3

---
+++ source/chart3.R
@@ -0,0 +1,26 @@
+# load packages
+library("wordcloud")
+library("tm")
+
+# load csv data
+twt_sentiment <- read.csv("https://raw.githubusercontent.com/info201b-au2022/Project-11-BC/main/data/Twitter-Sentiment-Dataset.csv", stringsAsFactors = FALSE)
+#convert to vector
+twt_text <- twt_sentiment$Cleaned.Tweets
+
+#convert to corpus
+twt_doc <- Corpus(VectorSource(twt_text))
+
+#clean text
+twt_doc <- twt_doc %>%
+  tm_map(removeNumbers) %>%
+  tm_map(removePunctuation) %>%
+  tm_map(stripWhitespace) 
+twt_doc <- tm_map(twt_doc, content_transformer(tolower))
+twt_doc <- tm_map(twt_doc, removeWords, stopwords("english"))
+ 
+#create wordcloud
+wordcloud(twt_doc,
+          max.words = 100,
+          random.order = FALSE,
+          colors = brewer.pal(8, "Dark2"))
+
