4f34177e9d182c5f7ba69a694d39940183a7b324
Wesley <serpents123@gmail.com>
2022-11-29 19:44:28 -0800

    some table changes

--- source/summary_table.R
+++ source/summary_table.R
@@ -6,8 +6,11 @@ twitter_data <- read.csv("https://raw.githubusercontent.com/info201b-au2022/Proj
 emotions_by_stress <- reddit_data %>%
   group_by(Stress) %>%
   summarise(
-    "Average Sentiment" = mean(Sentiment),
-    "Median Sentiment" = median(Sentiment),
+    "Average Sentiment (-1 to 1,
+    negative indicates negative sentiment, zero is neutral)" = mean(Sentiment),
+    "Median Sentiment (-1 to 1,
+    negative indicates negative sentiment,
+    zero is neutral)" = median(Sentiment),
     "Average Positive Emotion" = mean(Positive.Emotion),
     "Nedian Negative Emotion" = median(Positive.Emotion),
     "Average Negative Emotion" = mean(Negative.Emotion),
@@ -26,8 +29,10 @@ emotions_by_subreddit <- reddit_data %>%
     "Post Count" = n(),
     "Stressed" = sum(Stress == "Stressed"),
     "Stressed Percentage" = sum(Stress == "Stressed") / n(),
-    "Average Tone" = mean(Tone),
-    "Median Tone" = median(Tone)
+    "Average Tone (numbers indicate percentiles of stress,
+    lower indicates more stress)" = mean(Tone),
+    "Median Tone (numbers indicate percentiles of stress,
+    lower indicates more stress)" = median(Tone)
   )
 
 most_confident_neg_hugging <- twitter_data %>%
