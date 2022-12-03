b458528fc39fb7fddb708676ce6e4e4a0bf17eeb
katiestevens24 <katsierr@uw.edu>
2022-12-03 14:56:20 -0800

    chart1 & 3 edits - not finished

--- source/chart1.R
+++ source/chart1.R
@@ -5,15 +5,26 @@ library("ggplot2")
 
 #load csv data
 dreaddit <- read.csv("https://raw.githubusercontent.com/info201b-au2022/Project-11-BC/main/data/Dreaddit-Dataset.csv", stringsAsFactors = FALSE)
-
 # filter data
-dreaddit_data <-dreaddit %>%
+dreaddit_data <- dreaddit %>%
   group_by(Subreddit) %>%
-  summarize(Tone = mean(Tone), na.rm = TRUE)
+  summarize(Tone = mean(Tone), na.rm = TRUE) 
+
+dreaddit_data$Subreddit[dreaddit_data$Subreddit == "survivorsofabuse"] <- "Survivors of Abuse"
+dreaddit_data$Subreddit[dreaddit_data$Subreddit == "stress"] <- "Stress"
+dreaddit_data$Subreddit[dreaddit_data$Subreddit == "relationships"] <- "Relationships"
+dreaddit_data$Subreddit[dreaddit_data$Subreddit == "ptsd"] <- "PTSD"
+dreaddit_data$Subreddit[dreaddit_data$Subreddit == "homeless"] <- "Homeless"
+dreaddit_data$Subreddit[dreaddit_data$Subreddit == "food_pantry"] <- "Food Pantry"
+dreaddit_data$Subreddit[dreaddit_data$Subreddit == "domesticviolence"] <- "Domestic Violence"
+dreaddit_data$Subreddit[dreaddit_data$Subreddit == "assistance"] <- "Assistance"
+dreaddit_data$Subreddit[dreaddit_data$Subreddit == "anxiety"] <- "Anxiety"
+dreaddit_data$Subreddit[dreaddit_data$Subreddit == "almosthomeless"] <- "Almost Homeless"
 
 #create scatterplot
-chart1 = ggplot(data = dreaddit_data) +
-  geom_point(mapping = aes(x = Subreddit, y = Tone)) +
+chart1 <- ggplot(data = dreaddit_data) +
+  geom_col(mapping = aes(x = Subreddit, y = Tone)) +
   coord_flip() +
   labs(title = "Average Tone by Subreddit")
 
+chart1
--- source/chart3.R
+++ source/chart3.R
@@ -1,7 +1,7 @@
 # load packages
 library("wordcloud")
 library("tm")
-
+library("dplyr")
 # load csv data
 twt_sentiment <- read.csv("https://raw.githubusercontent.com/info201b-au2022/Project-11-BC/main/data/Twitter-Sentiment-Dataset.csv", stringsAsFactors = FALSE)
 #convert to vector
@@ -10,17 +10,31 @@ twt_text <- twt_sentiment$Cleaned.Tweets
 #convert to corpus
 twt_doc <- Corpus(VectorSource(twt_text))
 
-#clean text
-twt_doc <- twt_doc %>%
+docs <- twt_doc %>%
   tm_map(removeNumbers) %>%
   tm_map(removePunctuation) %>%
-  tm_map(stripWhitespace) 
-twt_doc <- tm_map(twt_doc, content_transformer(tolower))
-twt_doc <- tm_map(twt_doc, removeWords, stopwords("english"))
+  tm_map(stripWhitespace)
+docs <- tm_map(docs, content_transformer(tolower))
+docs <- tm_map(docs, removeWords, stopwords("english"))
+docs <- tm_map(docs, removeWords, c("will", "people", "stokes", "thinkwell", "ay", "yes", "indvseng", "pandya", "'m", "[][]", "'s", "said", "one", "just", "thats", "twitter", "ive", "hardik", "上海", "'re"))
+
+gsub("https\\S*", "", docs) 
+gsub("@\\S*", "", docs) 
+gsub("amp", "", docs) 
+gsub("[\r\n]", "", docs)
+gsub("[[:punct:]]", "", docs)
+
+#clean text
+library("tidytext")
+dtm <- TermDocumentMatrix(docs) 
+matrix <- as.matrix(dtm)
+words <- sort(rowSums(matrix), decreasing = TRUE)
+df <- data.frame(word = names(words), freq = words)
+
  
-#create wordcloud
-chart3 <- wordcloud(twt_doc,
-          max.words = 100,
-          random.order = FALSE,
-          colors = brewer.pal(8, "Dark2"))
+chart3 <- wordcloud(words = df$word, freq = df$freq, min.freq = 1,
+                    max.words = 100, random.order = FALSE, rot.per = 0.35,
+                    colors = brewer.pal(8, "Dark2"))
+chart3
+
 
