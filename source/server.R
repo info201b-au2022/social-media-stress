5142b9c64aa97982a51c6731b94b6520d76f0df0
Wesley <serpents123@gmail.com>
2022-12-05 01:51:17 -0800

    start basic interactive chart on general soc

--- source/server.R
+++ source/server.R
@@ -1,2 +1,21 @@
+library("plotly")
+library("tidyr")
+library("dplyr")
+library("stringr")
+
 server <- function(input, output) {
+  output$general_chart <- renderPlotly({
+    print(input$xvar)
+    to_plot <- pivot_longer(social_media, cols = input$xvar, names_to = "Selected", values_to = "Percentage")
+    chart2 <- ggplot(to_plot, aes(fill = Name, y = Name, x = Percentage)) +
+      geom_bar(position = "dodge", stat = "identity") +
+      labs(title = "Social Media Platforms",
+          x = input$xvar,
+          y = 'Platforms') +
+      theme(legend.position="none")
+
+    p <- ggplotly(chart2)
+
+    return(p)
+  })
 }
--- source/tabs/tab_chart1.R
+++ source/tabs/tab_chart1.R
@@ -1,3 +1,17 @@
+library(shiny)
+library(plotly)
+
+social_media <- read.csv("https://raw.githubusercontent.com/info201b-au2022/social-media-stress/main/data/General%20Social%20Media.csv", stringsAsFactors = TRUE, check.names=FALSE)
+
+x_input <- selectInput(
+  "xvar",
+  label = "X Variable",
+  choices = c('Monthly Active Users', 'Percentage of Users that are Female'),
+  selected = "Monthly Active Users"
+)
+
 interactive1 <- tabPanel(
-  "Interactive 1"
+  "Interactive 1",
+  x_input,
+  plotlyOutput("general_chart")
 )
