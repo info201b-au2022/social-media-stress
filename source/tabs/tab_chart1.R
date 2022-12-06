3cd819668bf31e51f4878144be5d017fdf339130
Wesley <serpents123@gmail.com>
2022-12-05 23:18:09 -0800

    kinda finalize chart 1

--- source/tabs/tab_chart1.R
+++ source/tabs/tab_chart1.R
@@ -1,17 +1,27 @@
 library(shiny)
 library(plotly)
 
-social_media <- read.csv("https://raw.githubusercontent.com/info201b-au2022/social-media-stress/main/data/General%20Social%20Media.csv", stringsAsFactors = TRUE, check.names=FALSE)
+social_media <- read.csv("https://raw.githubusercontent.com/info201b-au2022/social-media-stress/main/data/General%20Social%20Media.csv", stringsAsFactors = TRUE, check.names = FALSE)
 
 x_input <- selectInput(
   "xvar",
-  label = "X Variable",
-  choices = c('Monthly Active Users', 'Percentage of Users that are Female'),
+  label = "Choose a variable to graph on:",
+  choices = c("Monthly Active Users",
+  "Percentage of Users that are Female",
+  "Percentage of Users that are Male",
+  "Daily Time Spent (in minutes)",
+  "Growth in 2020",
+  "Growth in 2021"),
   selected = "Monthly Active Users"
 )
 
 interactive1 <- tabPanel(
   "Interactive 1",
-  x_input,
-  plotlyOutput("general_chart")
+  tags$section(
+    x_input,
+    p(HTML("&emsp;"), "This chart displays the most popular social media
+    apps. You can change the variable to be able to alter the variable graphed
+     on the X axis."),
+  ),
+  plotlyOutput("general_chart"),
 )
