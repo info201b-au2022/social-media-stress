c4904879cdbcbcc1d60d6ea4a1e9b32d2d7b34cb
Lydial14 <lydial14@uw.edu>
2022-12-07 22:59:50 -0800

    graph 3 description:"

--- source/tabs/tab_chart3.R
+++ source/tabs/tab_chart3.R
@@ -20,6 +20,16 @@ interactive3 <- tabPanel(
   tags$section (
     stress_input,
     variable_input,
+    p(HTML("&emsp;"), "This chart analyzes three variables from the Dreaddit dataset
+      : Tone, Positive Emotion, and Negative Emotion. The Reddit datasets are also
+      grouped into two groups based on calculated stress status: Stressed and not
+      Stressed. Tone is calculated on a scale of 0 to 100. A lower numerical tone
+      value corresponds to a higher level of stress detected in the 10 subreddits
+      analyzed, while a higher numerical tone indicates a lower level of stress 
+      detected. As for positive and negative emotion, a higher numerical value 
+      correlates to a higher presence of the variable being analyzed. (For example
+      subreddits that have a positve emotion value of 5, contain more positivity
+      in the posts than subreddits with a positve emotion value under 5.)"),
   ),
   plotlyOutput("plot3")
 )
