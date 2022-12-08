0ff8dba7dbdcac7ff19a47cefa6800fa1d0348ce
Wesley <serpents123@gmail.com>
2022-12-08 09:53:21 -0800

    fill out some conclusion

--- source/tabs/tab_conclusion.R
+++ source/tabs/tab_conclusion.R
@@ -1,3 +1,71 @@
 conclusion <- tabPanel(
   "Conclusion",
+  tags$section(
+    h3("Findings"),
+    tags$div(
+      tags$div(
+        h3("Discussion"),
+        p(HTML("&emsp;"), "With the aboe findings, there is an importance of recognizing that different factors
+  come to play with the contributions of stress that is experienced when utilizing social media. Social media
+  can be utilized in many different ways, whether it be for entertainment, for advocation, for social connection,
+  or even to gain important information such as the news."),
+        p(HTML("&emsp;"), "However in order to figure out the underlying reasoning as to how much of an impact social media is
+  a stressor in an individual’s mental well being, it is always important to taken into account that with the
+  various different ways social media can be utilized, different age groups interact with social media
+  in different ways."),
+        p(HTML("&emsp;"), "Younger generations may view social media as a much more positive tool within their
+  every day lives, where as the older generations may view social media as a negative or unimportant aspect
+  of their lives. Because of the difference in which social media is viewed within the minds of the different
+  age groups, social media may already be considered a stressor for individuals. For the younger generation,
+  social media can be viewed as a way to cope with their mental wellbeing, and may view the power of social
+  media as a positive environment, whereas the older generation may view social media as a waste of time and
+  overall just a stressful environment to be in."),
+        p(HTML("&emsp;"), "With the fact that different age groups have different
+  perspectives on social media, it must be considered that stress from social media can indeed be influenced
+  with presumed perspectives. It was reflected within the findings that a significant number of individuals
+  had responded that social media had neither a purely positive or negative impact on their mental wellbeing.
+  For the Generation Z and the Millennial group, social media is viewed as convenient way to gain information
+  and socialize, some stressors from social media for this group may come from the content they are receiving
+  such as sensitive topics of on going affairs, but overall, social media for these age groups is used as a
+  means to connect and share what is going on in their lives. This is again a reflection of the majority’s
+  perspective, the Generation Z and the Millennial group that has moreover a positive view on the utilization of
+  social media, as it is a vital part of their everyday lives."),
+      ),
+      img(
+        src = "https://raw.githubusercontent.com/info201b-au2022/social-media-stress/main/source/tabs/img/Pie_Chart.jpg",
+      )
+    ),
+    tags$div(
+      tags$div(
+        h3("Conclusion"),
+        p(HTML("&emsp;"), "When looking at the data sets there are several different points in which it is important
+  to take into account. It was recognized that when taking information from the dataset it was crucial to remember
+  that a cultural aspect had an impact on the information that was received and stated. With the topic of the
+  correlation of stress and social media, and the specific data that was retrieved for the findings, there was a
+  demographic bias. Thus meaning that the demographic in which the data was retrieved from was coming from mostly
+  the Generation X and the Millennial age group. More so specifically, the retrieved findings from the data is more
+  of a reflection of the correlation of stress and social media on the Generation X and Millennial group, not a
+  reflection of all individuals who utilize social media. With the findings, it is important to emphasize that
+  different perspectives are held about social media when comparing different age groups. For example, Generation
+  X and Millennial groups find that social media holds a significant importance to their everyday lives and often
+  viewed as a positive way to connect with others, and that older generation groups may look at social media as a
+  daunting platform which is complex and stressful in their perspectives. With different perspectives on the importance
+  and influence of social media, it is important to consider that the factors that contribute to stress experienced
+  as a result of social media also differ from generation to generation. For example, stress gained from social media
+  can look for a Generation X individual compared to a Baby Boomer individual. For an individual from the Generation X
+  era, the presence of social media has been present for almost half of their lives and view social media as a platform
+   to socialize. For Generation x individuals social media plays a vital part in their lives, as most of their
+   information and connection with others is through the internet, as well as utilize social media as their main
+   source of news. On the other hand, for Baby Boomers, social media has not been a vital part of their lives,
+   not many of the Baby Boomer population will utilize social media in general, and will most likely utilize
+   social media only for entertainment purposes and to just share content about their lives. Therefore, when
+   considering whether or not stress really does or does not have a correlation with social media, the
+   perspectives of different age groups and demographic bias must always be considered when looking at the
+   data sets."),
+      ),
+      img(
+        src = "https://raw.githubusercontent.com/info201b-au2022/social-media-stress/main/source/tabs/img/Pie_Chart.jpg",
+      )
+    ),
+  )
 )
