2bb2aa16ae6bf2f9d54aef37276aa39101c2e057
Wesley <serpents123@gmail.com>
2022-12-07 21:35:15 -0800

    format chart2

--- source/tabs/tab_chart2.R
+++ source/tabs/tab_chart2.R
@@ -7,17 +7,19 @@ survey_plot <- survey_csv %>%
 x_input <- selectInput(
   inputId = "x_var",
   label = "Choose an aspect of daily life:",
- choices = c('Access to information', 'Ease of communication', 'Worsened personal privacy', 'Distractions during your day', 'Polarization in politics', 
+ choices = c('Access to information', 'Ease of communication', 'Worsened personal privacy', 'Distractions during your day', 'Polarization in politics',
              'Foreign meddling in politics', 'Censorship', 'Your overall quality of life', 'Civility in culture', 'Civility online' ),
  selected = TRUE
 )
 
 interactive2 <- tabPanel(
   "Interactive 2",
-  x_input,
+  tags$section(
+    x_input,
+    p(HTML("&emsp;"), "This chart displays the measure of social media impact on daily life of individuals ages 16-64 worldwide as of February 2019. Respondents
+    were asked to repond whether different aspects of their daily life had decreased, increased, or had no impact as a result of social media.
+    You can change the variable to be able to alter the variable graphed on the Y axis."),
+  ),
   plotlyOutput("survey_chart"),
-  p(HTML("&emsp;"), "This chart displays the measure of social media impact on daily life of individuals ages 16-64 worldwide as of February 2019. Respondents 
-  were asked to repond whether different aspects of their daily life had decreased, increased, or had no impact as a result of social media. 
-  You can change the variable to be able to alter the variable graphed on the Y axis.")
 )
 
